d28ab7cf3f5e9eeff42df1bc61e4a9ade417421d
Krishnachandran U <129476971+krishnachandran-u@users.noreply.github.com>
2024-09-02 06:47:12 +0530

    Add files via upload

---
+++ s5/mp/add16.asm
@@ -0,0 +1,30 @@
+.MODEL small         ; Define the memory model as small
+.STACK 100h          ; Set the stack size to 256 bytes
+
+.data
+num1 dw 1234h
+num2 dw 5678h
+result dw 0
+carry_flag db 0
+        
+.code 
+main proc
+    mov ax, @data
+    mov ds, ax
+    mov ax, num1
+    add ax, num2
+    mov result, ax
+    jc carry_occurred:
+    mov carry_flag, 0
+    jmp done:
+    
+carry_occurred:
+    mov carry_flag, 1
+
+done:
+    mov result, ax
+    mov ah, 4ch
+    int 21h
+
+main endp
+end main
