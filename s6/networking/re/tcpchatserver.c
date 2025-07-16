407ebc30669845c4794c2cc927a8d1b7c95834e6
Krishnachandran U <kriss52450@gmail.com>
2025-07-16 22:06:21 +0530

    idk

--- s6/networking/re/tcpchatserver.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <stdbool.h>
-#include <arpa/inet.h>
-#include <
--- s6/networking/re/timeserver.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <netdb.h>
-#include <sys/socket.h>
-#include <sys/types.h>
